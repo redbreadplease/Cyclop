74142199627fde1a9adf0384c8848b337332d7bc
redbreadplease <smailden4@gmail.com>
2020-01-07 02:36:00 +0300

    Added bottom navigation bar

--- app/src/main/java/com/example/spacenews/MainActivity.kt
+++ app/src/main/java/com/example/spacenews/MainActivity.kt
@@ -7,9 +7,11 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.spacenews.recyclerview.RecyclerViewAdapter
 import com.example.spacenews.retrofit.pojos.SpaceNewsPost
 import com.example.spacenews.retrofit.NetworkService
+import kotlinx.android.synthetic.main.activity_main.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import kotlin.concurrent.thread
 
 
 class MainActivity : AppCompatActivity() {
@@ -21,8 +23,27 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        setRecyclerView()
-        showNewsTitles()
+        tryToShowNews()
+
+        tuneBottomNavbar()
+    }
+
+    private fun tryToShowNews() {
+        thread {
+            setRecyclerView()
+            showNewsTitles()
+            if (adapter == null)
+                showNewsTitles()
+        }
+    }
+
+    private fun tuneBottomNavbar() {
+        bottom_navigation_view.setTextVisibility(false)
+        bottom_navigation_view.enableItemShiftingMode(false)
+        bottom_navigation_view.enableShiftingMode(false)
+        bottom_navigation_view.enableAnimation(false)
+        for (i in 0 until bottom_navigation_view.menu.size())
+            bottom_navigation_view.setIconTintList(i, null)
     }
 
     private fun setAdapter(list: MutableList<String>) {
@@ -40,7 +61,10 @@ class MainActivity : AppCompatActivity() {
             .getJSONApi()
             ?.getPosts()
             ?.enqueue(object : Callback<List<SpaceNewsPost?>> {
-                override fun onResponse(call: Call<List<SpaceNewsPost?>>, response: Response<List<SpaceNewsPost?>>) {
+                override fun onResponse(
+                    call: Call<List<SpaceNewsPost?>>,
+                    response: Response<List<SpaceNewsPost?>>
+                ) {
                     println("!!!!It\'s all ok!")
                     val posts: List<SpaceNewsPost?>? = response.body()
                     val content = mutableListOf<String>()
